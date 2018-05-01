7368af0b864a6f4dbc152fdca76755bc1d051d04
Alex Palmitjavila <alex.palmitjavila@epitech.eu>
2018-05-01 12:29:43 +0200

    Added /allocine/:id where :id is theater ID

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const request = require('request');
+const allocine = require('allocine-api');
 
 var app = express();
 
@@ -15,32 +16,16 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
- app.get("/allocine/:id", function(req, res) {
+app.get("/allocine/:id", function(req, res) {
   var cinema = req.params.id;
- });
+  allocine.api('showtimelist', {theaters: cinema}, function(error, results) {
+    if(error) { console.log('Error : '+ error); return; }
 
-app.get("/allocine", function(req, res) {
-  var o = {} // empty Object
-  var key = 'Names';
-  o[key] = []; // empty Array, which you can push() values into
+    console.log('Voici les données retournées par l\'API Allociné:');
+    console.log(results);
+    res.status(200).json(results);
 
-
-  var data = {
-    city: 'montpellier',
-    data: 'Multiplexe Odysseum'
-  };
-  var data2 = {
-    city: 'montpellier',
-    data: 'Comedie'
-  };
-  o[key].push(data);
-  o[key].push(data2);
-  console.log(JSON.stringify(o));
-  res.status(200).json(o);
+  });
 });
 
 function callAPI(url, callback) {
