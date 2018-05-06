87dec61eb8a902457e516af5fd71d3b14ce2a3e1
Paolo-Gambardella <paolo.gambardella@epitech.eu>
2018-05-06 16:34:28 +0200

    Widgets without list works

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: latlong[0], lng: latlong[1]};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 7,
         center: uluru
     });
     var marker = new google.maps.Marker({
