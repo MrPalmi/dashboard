d72de49341aa555080f5f92261e092cbd773ec93
Alex Palmitjavila <alex.palmitjavila@epitech.eu>
2018-05-12 22:09:28 +0200

    Server error managment

--- index.js
+++ index.js
@@ -34,11 +34,7 @@ app.get("/allocine/:id", function(req, res) {
   var cinema = req.params.id;
   allocine.api('showtimelist', {theaters: cinema}, function(error, results) {
     if(error) { console.log('Error : '+ error); return; }
-
-    if (results['feed']['totalResults'] == 0)
-      res.status(200).json({"error": 1, "message": "Le cinéma n'existe pas"});
-    else
-      res.status(200).json(results);
+    res.status(200).json(results);
   });
 });
 
@@ -56,16 +52,9 @@ app.get("/steam/:id", function(req, res) {
 
   var userStats = new SteamApi.UserStats('steam-api-key');
 
-  try {
-      userStats.GetNumberOfCurrentPlayers(game).done(function(result){
-        res.status(200).json(result);
-      });
-  }
-  catch(e) {
-    console.log(e);
-  // expected output: "Parameter is not a number!"
-  }
- 
+  userStats.GetNumberOfCurrentPlayers(game).done(function(result){
+    res.status(200).json(result);
+  });
 });
 
 app.get("/steam", function(req, res) {
@@ -93,7 +82,7 @@ app.get("/lgtandlat/:id", function(req, res) {
     if (data["status"] == "OK") {
       res.json(data["results"][0]["geometry"]["location"]);
     } else {
-      res.json(data["status"]);
+      res.json({error: 1, message: "Impossible de trouver latitude et longitude de la recherche"});
     }
   });
 });
@@ -104,7 +93,10 @@ app.get("/weather/:id", function(req, res) {
   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=3ee58512b501c5a957e02833caa95404";
 
   var body = callAPI(url, function(data) {
-    res.json(data);
+    if (data["cod"] == 404)
+      res.json({error: 1, message: "Impossible de trouver l'emplacement de la recherche"});
+    else
+      res.status(200).json(data);
   });
 });
 
@@ -121,7 +113,11 @@ app.get("/time/:id", function(req, res) {
     var nurl = "http://api.timezonedb.com/v2/get-time-zone?key=7C8ZRCKIUQKD&format=json&by=position&lng=" + lng + "&lat=" + lat;
 
     var bbody = callAPI(nurl, function(ddata) {
-      res.json(ddata);
+      if (ddata["status"] == "OK") {
+        res.json(ddata);
+      } else {
+        res.json({error: 1, message: "Impossible de trouver les coordonées de la recherche"});
+      }
     });
   });
 });
@@ -168,7 +164,7 @@ app.get("/redditsubcount/:id", function(req, res) {
     if (data['data']['children'][0]) {
       res.json(data['data']['children'][0]['data']["subreddit_subscribers"]);
     } else {
-      res.json({error:"Couldn't fetch subscriber count"})
+      res.json({error: 1, message: "Impossible de récuperer le nombre d'abonnées au subreddit"})
     }
   });
 });
@@ -180,7 +176,7 @@ app.get("/subreddit/:id", function(req, res) {
     if (data['data']['children'][0]) {
       res.json(data);
     } else {
-      res.json({error:"Couldn't fetch subscriber count"})
+      res.json({error: 1, message: "Impossible de récuperer les posts du subreddit"})
     }
   });
 });
